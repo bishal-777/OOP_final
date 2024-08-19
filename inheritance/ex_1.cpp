50b4ce35ceb5b55c4a113ae21b40e86156ec9335
Bishal <bishal777giri@gmail.com>
2024-08-20 05:30:26 +0545

    Added 1 program with operator overloading and another with inheritance

---
+++ inheritance/ex_1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class Cuboid{
+    protected:
+    int l,b,h;
+    public:
+    void getData(int x,int y,int z){
+        l=x;
+        b=y;
+        h=z;
+    }
+};
+
+class Area:public Cuboid{             
+    public:
+    int tsa(){
+        int a=2*(l*b+b*h+l*h);      //total surface area
+        return a;
+    }  
+    int lsa(){
+        int a=2*(l+b)*h;            //lateral surface area
+        return a;
+    }
+};
+
+class Volume:public Cuboid{
+    public:
+    int vol(){
+        int v=l*b*h;
+        return v;
+    }
+};
+
+int main(){
+    Area c;
+    Volume v;
+    c.getData(1,2,3);
+    v.getData(1,2,3);
+    cout<<"Area:\n"<<"Total surface area:\t"<<c.tsa()<<"\nLateral surface area:\t"<<c.lsa()<<endl;
+    cout<<"\n\nVolume:"<<v.vol()<<endl;
+    return 0;
+}
