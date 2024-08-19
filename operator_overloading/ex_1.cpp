3577a1a301366a26309f14e369b77d7b9cdd4b9a
Bishal <bishal777giri@gmail.com>
2024-08-19 19:42:19 +0545

    first commit

---
+++ operator_overloading/ex_1.cpp
@@ -0,0 +1,23 @@
+//This program changes the sign of input number from positive to negative and vice versa
+#include<iostream>
+using namespace std;
+class Change{
+    int num;
+    public:
+    Change(int n){
+        num=n;
+    }
+    void operator -(){
+        num=-num;
+    }
+    void display(){
+        cout<<"The changed integer is:"<<num<<endl;
+    }
+};
+
+int main(){
+    Change c1(5);
+    -c1;
+    c1.display();
+    return 0;
+}
